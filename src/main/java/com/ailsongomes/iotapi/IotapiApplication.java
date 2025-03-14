0a648c6e0b98014aad9aa87ee88fb7f655738e5b
arilson <garilson2002@example.com>
2025-03-14 21:06:55 -0100

    first commit

---
+++ src/main/java/com/ailsongomes/iotapi/IotapiApplication.java
@@ -0,0 +1,13 @@
+package com.ailsongomes.iotapi;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class IotapiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(IotapiApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/ailsongomes/iotapi/IotapiApplicationTests.java
@@ -0,0 +1,13 @@
+package com.ailsongomes.iotapi;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class IotapiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
