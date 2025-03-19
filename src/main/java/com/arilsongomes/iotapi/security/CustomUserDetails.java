88b5e359c0c95837d8e8e38d73051c699147c558
arilson <garilson2002@example.com>
2025-03-19 16:12:47 -0100

    feat: Implements authentication and authorization using Spring Security: CustomUserDetails and CustomUserDetailsService

---
+++ src/main/java/com/arilsongomes/iotapi/security/CustomUserDetails.java
@@ -0,0 +1,61 @@
+package com.arilsongomes.iotapi.security;
+
+
+import com.arilsongomes.iotapi.entity.User;
+import lombok.Data;
+import lombok.Getter;
+import org.springframework.security.core.GrantedAuthority;
+import org.springframework.security.core.userdetails.UserDetails;
+
+import java.util.Collection;
+import java.util.List;
+
+@Data
+public class CustomUserDetails implements UserDetails {
+    @Getter
+    public Long id;
+    private String email;
+    private String password;
+    private Collection<? extends GrantedAuthority> authorities;
+
+    public CustomUserDetails(User user) {
+        id = user.getId();
+        password = user.getPassword();
+        email = user.getEmail();
+    }
+
+    @Override
+    public Collection<? extends GrantedAuthority> getAuthorities() {
+        return authorities;
+    }
+
+    @Override
+    public String getPassword() {
+        return password;
+    }
+
+    @Override
+    public String getUsername() {
+        return email;
+    }
+
+    @Override
+    public boolean isAccountNonExpired() {
+        return true;
+    }
+
+    @Override
+    public boolean isAccountNonLocked() {
+        return true;
+    }
+
+    @Override
+    public boolean isCredentialsNonExpired() {
+        return true;
+    }
+
+    @Override
+    public boolean isEnabled() {
+        return true;
+    }
+}
--- src/main/java/com/arilsongomes/iotapi/services/user/UserServiceImpl.java
+++ src/main/java/com/arilsongomes/iotapi/services/user/UserServiceImpl.java
@@ -4,18 +4,26 @@ import com.arilsongomes.iotapi.dto.UserDto;
 import com.arilsongomes.iotapi.entity.User;
 import com.arilsongomes.iotapi.repositories.UserRepository;
 import lombok.RequiredArgsConstructor;
+import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.stereotype.Service;
+
+import java.util.List;
 
 @Service
 @RequiredArgsConstructor
 public class UserServiceImpl implements UserService{
     private final UserRepository userRepository;
+    private final PasswordEncoder passwordEncoder;
 
     @Override
     public User createUser(UserDto dto) {
         User user = new User();
         user.setEmail(dto.email());
-        user.setPassword(dto.password());
+        user.setPassword(passwordEncoder.encode(dto.password()));
         return userRepository.save(user);
+    }
+
+    public List<User> getAllUser(){
+        return userRepository.findAll();
     }
 }
