e7e736880b101ed77455aed9591de29fb04c4eb0
arilson <garilson2002@example.com>
2025-03-19 14:59:16 -0100

    feat: Implement global exception handling logic

---
+++ src/main/java/com/arilsongomes/iotapi/exceptions/GlobalExceptionHandler.java
@@ -0,0 +1,30 @@
+package com.arilsongomes.iotapi.exceptions;
+
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.MethodArgumentNotValidException;
+import org.springframework.web.bind.annotation.ControllerAdvice;
+import org.springframework.web.bind.annotation.ExceptionHandler;
+
+import java.time.LocalDateTime;
+import java.util.List;
+import java.util.stream.Collectors;
+
+@ControllerAdvice
+public class GlobalExceptionHandler {
+
+    @ExceptionHandler(MethodArgumentNotValidException.class)
+    public ResponseEntity<ErrorDetails> handleValidationErros(MethodArgumentNotValidException ex){
+        List<String> errors = ex.getBindingResult()
+                .getFieldErrors()
+                .stream()
+                .map(fieldError -> fieldError.getField() + ": " + fieldError.getDefaultMessage())
+                .toList();
+
+        ErrorDetails errorDetails = new ErrorDetails(
+                LocalDateTime.now(), "Validation error", errors.toString()
+        );
+
+        return new ResponseEntity<>(errorDetails, HttpStatus.BAD_REQUEST);
+    }
+}
